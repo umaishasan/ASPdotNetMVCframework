92d2b363d638b55ae41b42c43aad21339d9ee881
umais <umais@H-U-H>
2021-08-01 18:58:59 +0500

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,61 @@
+﻿using ASPdotNetMVCframework.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPdotNetMVCframework.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductEntities dbObj = new ProductEntities();
+        // GET: Product
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(ProductTable myModel)
+        {
+            ProductTable dbTableobj = new ProductTable();
+            dbTableobj.productName = myModel.productName;
+            dbTableobj.productPrice = myModel.productPrice;
+            dbObj.ProductTables.Add(dbTableobj);
+            dbObj.SaveChanges();
+            ModelState.Clear();
+            return View("Product");
+        }
+
+        public ActionResult ProductList()
+        {
+            var res = dbObj.ProductTables.ToList();
+            return View(res);
+        }
+
+        
+        public ActionResult deleteProduct(int projectId)
+        {
+            var rest = dbObj.ProductTables.Where(x=>x.productId == projectId).First();
+            dbObj.ProductTables.Remove(rest);
+            dbObj.SaveChanges();
+
+            var listres = dbObj.ProductTables.ToList();
+            return View("ProductList", listres);
+        }
+
+        
+        public ActionResult updateProduct(ProductTable objTbl)
+        {
+            if(objTbl != null)
+            {
+                return View(objTbl);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPdotNetMVCframework.MvcApplication" Language="C#" %>
